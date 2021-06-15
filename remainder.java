cef8cff55af3af43cc55fbeaae8ddf4818088a73
snehamahalingam123 <84567458+snehamahalingam123@users.noreply.github.com>
2021-06-16 04:23:45 +1400

    initial commit

---
+++ remainder.java
@@ -0,0 +1,16 @@
+
+public class remainder {
+
+	public static void main(String[] args) {
+		int n1 =22;
+		int n3 = 10;
+		double n2 = 22.25;
+		int remainder = n1 % n3;
+		double remainder2 = n2 % n3;
+		System.out.println("remainder: " + remainder);
+		System.out.println("remainder 2 : " + remainder2);
+
+	}
+
+}
+ 
